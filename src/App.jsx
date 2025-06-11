0e61706d1d98851cff0294e8a9f720d9c7ef2bee
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-11 12:14:14 +0200

    0.2v

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { useParams } from 'react-router-dom';
+import { Routes, useParams } from 'react-router-dom';
 import './App.css';
 
 // React dont support Routing to that we need to add library
@@ -135,7 +135,8 @@ import './App.css';
 // if we had 5 difireint BlogPosts we dont need to create route to each of
 // them we can add :postId and user can cavigate with url with names of post
 
-<Route path="/blog/:postId" element={<BlogPost />} />;
+{
+  /* <Route path="/blog/:postId" element={<BlogPost />} />;
 
 import { useParams } from 'react-router-dom';
 
@@ -178,14 +179,160 @@ export const AppLink = () => {
         <Route path="/products" element={<Products />} />
         <Route path="*" element={<NotFound />} />
 
-        {/* Lets add route to products by its id  */}
-        <Route path="/products/:productsId" element={<ProductDetails />} />
+        {/* Lets add route to products by its id  */
+}
+//         <Route path="/products/:productsId" element={<ProductDetails />} />
 
-        {/*  */}
-      </Routes>
+//         {/*  */}
+//       </Routes>
+//     </div>
+//   );
+// }; */}
+
+// ===============================================================================================
+
+// hook useParams() return all dinamic that are in current adress.
+// name of param is its property name for example if path /books/:genreId/:authorName
+// was declared and user is under /books/adventure/herman-melville.
+
+// const { genreId, authorName } = useParams();
+// console.log(genreId, authorName);
+
+// //
+
+// import { useParams } from 'react-router-dom';
+
+// const ProductDetails = () => {
+//   const { productId } = useParams();
+//   return <div>Now showing product with id - {productId}</div>;
+// };
+
+// import { useParams } from 'react-router-dom';
+// import { getProductById } from '../fakeApi';
+
+// export default function ProductDetails() {
+//   const { id } =useParams();
+//   const product = getProductById(id);
+
+//   return (
+//     <main>
+//       <img src="https://via.placeholder.com/960x240" alt=''/>
+//       <div>
+//         <h2>
+//           Product - {product.name} - {id}
+//         </h2>
+//         <p>
+//            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
+//           sunt excepturi nesciunt iusto dignissimos assumenda ab quae cupiditate
+//           a, sed reprehenderit? Deleniti optio quasi, amet natus reiciendis
+//           atque fuga dolore? Lorem, ipsum dolor sit amet consectetur adipisicing
+//           elit. Impedit suscipit quisquam incidunt commodi fugiat aliquam
+//           praesentium ipsum quos unde voluptatum?
+//         </p>
+//       </div>
+//     </main>
+//   )
+// }
+
+//============================================================================================
+
+// if we have components inside a component and we want to direct it in there we dont do something like this
+
+{
+  /* <Routes>
+<Route path="/about" element={<About />} />
+<Route path="/about/mission" element={<Mission />} />
+<Route path="/about/team" element={<Team />} />
+<Route path="/about/reviews" element={<Reviews />} />
+</Routes> */
+}
+
+// this will create for difrient pages about
+// to do it properly if about is already defined we can just add path to new component
+
+{
+  /* <Routes>
+<Route path="/about" element={<About />} />
+<Route path="mission" element={<Mission />} />
+<Route path="team" element={<Team />} />
+<Route path="reviews" element={<Reviews />} />
+</Routes> */
+}
+
+// this is proper way tp direct pages with use of relative path
+
+// Example
+
+// import { Routes, Route, Link } from 'react-router-dom';
+// import Home from 'path/to/pages/Home';
+// import About from 'path/to/pages/About';
+// import Products from 'path/to/pages/Products';
+// import NotFound from 'path/to/pages/NotFound';
+// import ProductDetails from 'path/to/pages/ProductDetails';
+// import Mission from 'path/to/components/Mission';
+// import Team from 'path/to/components/Team';
+// import Reviews from 'path/to/components/Reviews';
+
+// export const AppShop = () => {
+//   return (
+//     <div>
+//       <nav>
+//         <Link to="/">Home</Link>
+//         <Link to="/about">About</Link>
+//         <Link to="/products">Home</Link>
+//       </nav>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="mission" element={<Mission />} />
+//         <Route path="team" element={<Team />} />
+//         <Route path="reviews" element={<Reviews />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/products/:productId" element={<ProductDetails />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// ============================================================================================
+//
+// OUTLET
+//
+// if we wnat to render element on same page after clicking it we can use <outlet/>
+// its a marker where we want our element to render
+//
+import { Link, Outlet } from 'react-router-dom';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+      <ul>
+        <li>
+          <Link to="mission">Read about our mission</Link>
+        </li>
+        <li>
+          <Link to="team">Get to know the team</Link>
+        </li>
+        <li>
+          <Link to="reviews">Go through the reviews</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
   );
 };
+// ======================================================================================
+
+// if we want to routing to work properly we neeed to add Vercel and vercel.json config file 
+
+// and this is how it should look like 
+// {
+//   "rewrites":  [
+//     {"source": "/(.*)", "destination": "/"}
+//   ]
+// }
 
 function App() {}
 
---
+++ src/AppNavigate.jsx
@@ -0,0 +1,32 @@
+// Router not only allow to navigate after clicking link but also after 
+// specific action of user after event or after effect.
+// For example after user have loged in we can redirect page to user profile 
+// but only after request is susccesfull 
+
+// import { useNavigate } from "react-router-dom";
+
+// export const Login = () => {
+//     const navigate = useNavigate();
+
+//     const handleSubmit = async values => {
+//         const response = await FakeAPI.login(values);
+//         if (response.success) {
+//             navigate('/profile', {replace: true});
+//         }
+//     };
+
+//     return (
+//         <div>
+//             <h1>Login page</h1>
+//             <LoginForm onSubmit={handleSubmit} />
+//         </div>
+//     )
+// }
+
+// replace is very usefull propety it render profile on top of component 
+// because user after loging to site dont have any thing to do on loging page 
+// and so user could not return to login page with back key 
+
+// replace is easy to use and short in code 
+// to do the same we would have to useState but it is much longer then just replace 
+
